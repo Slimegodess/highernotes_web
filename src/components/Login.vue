9cca5bb5502959fbc4befe8dc60397a1fcc859a1
Slimegodess <haidangbui96@gmail.com>
2021-06-24 13:35:58 +0200

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="authentication">
+      <div v-if="authState !== 'signedin'">You are signed out.</div>
+      <amplify-authenticator>
+        <div v-if="authState === 'signedin' && user">
+          <div>Hello, {{user.username}}</div>
+        </div>
+        <amplify-sign-out></amplify-sign-out>
+      </amplify-authenticator>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'authentication',
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/PageLoader.vue
@@ -0,0 +1,28 @@
+<template>
+  <transition>
+    <div class="preloader">
+      <div class="logo"></div>
+      //do something
+      //if loaded play video
+    </div>
+    <p> LOADING SCREEN</p>
+  </transition>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style >
+  display: flex
+  flex-direction: column
+  align-items: center
+  justify-content: center
+  position: absolute
+  width: 100%
+  heigth: 100%
+  background-color: black
+  z-index: 9999
+</style>
