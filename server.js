883cef59061f3c5d3b9e3558b18b87e8d01b9952
Slimegodess <haidangbui96@gmail.com>
2021-06-26 13:40:31 +0200

    make it better

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const express = require("express");
-const port = process.env.PORT || 8080;
+// eslint-disable-next-line import/no-extraneous-dependencies
+const express = require('express');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const serveStatic = require('serve-static');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + "/dist/"));
-app.get(/.*/, function(req, res) {
-  res.sendfile(__dirname + "/dist/index.html");
-});
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+
+const port = process.env.PORT || 8080;
 app.listen(port);
 
-console.log("Server started...");
+console.log('Server started...');
